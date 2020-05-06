ed9a2f70c1c98b0dd5c8adc149498e4a2a3bdc44
RinaLia <loa.orlando@gmail.com>
2020-05-06 22:33:26 +0700

    tidak keluar hasil

--- src/function.js
+++ src/function.js
@@ -1,4 +1,4 @@
-const totalDistance = (start, end) => {
+const getDistance = (start, end) => {
   //inisiasi
   let speed = 6;
   let totalDistance = 0;
@@ -11,28 +11,29 @@ const totalDistance = (start, end) => {
   const diffSecond = (endDate - startDate) / 1000;
   const time = (sec) => new Date(startDate + sec * 1000);
 
-  //perulangan
+  // Perulangan
   for (var i = 1; i <= diffSecond; i++) {
     let timeInMinute = i / 60;
     const currentTime = `${time(i).getHours()}:${time(i).getMinutes()}:${time(
       i
     ).getSeconds()}`;
 
-    if (timeInMinute === 5) {
-      console.log(currentTime);
-      console.log(timeInMinute, "minutes bertamabah 2ms");
+    // Cek 5 Menit pertama
+    if (i === 300) {
+      // console.log(currentTime);
+      console.log(currentTime, timeInMinute, "minutes bertamabah 2ms");
       speed += 2;
     }
-
-    if ((timeInMinute - 5) % 10 === 0) {
-      if (timeInMinute !== 5) {
-        console.log(currentTime);
-        console.log(timeInMinute, "minutes bertamabah 2ms");
-        speed++;
+    // Cek setiap 10 Menit
+    else if ((i - 300) % 600 === 0) {
+      if (i !== 300) {
+        //   console.log(currentTime);
+        console.log(currentTime, timeInMinute, "minutes bertamabah 1ms");
+        speed += 1;
       }
     }
-    totalDistance += speed;
   }
   return totalDistance;
 };
-module.export = totalDistance;
+
+module.exports = getDistance;
